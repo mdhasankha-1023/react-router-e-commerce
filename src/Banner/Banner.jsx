68d6fbd53163a7f13ba6b6fb28d8be0676b41419
mdhasankhan-123 <hasankha1023@gmail.com>
2023-04-22 01:11:54 +0600

    banner complited

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -16,9 +16,16 @@ const Banner = () => {
         } , 5000)
         return () => clearInterval(interval)
     } ,[currentImg]);
+    console.log(img[currentImg])
     return (
-        <div>
-            <img className='bnr-img' src={img[currentImg]} alt="banner-img" />
+        <div className='bnr-container'>
+            <img className='bnr-img' src={img[currentImg]} alt="" /> 
+            <div className="bnr-text">
+                <p className='text-2xl bg-red-500 text-white inline px-2'>Best Prices</p>
+                <h2 className='text-7xl py-4 font-semibold leading-tight'>Incredible Prices<br/> on All Your<br />Favorite Items</h2>
+                <h5 className='text-2xl my-3'>Get more for less on selected brands</h5>
+                <button className='px-16 py-4 bg-purple-600 rounded-full text-white text-xl mt-8'>Shop now</button>
+            </div>
         </div>
     );
 };
